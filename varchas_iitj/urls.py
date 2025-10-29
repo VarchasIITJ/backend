bd1cbc2571dd674e11a5e893f82b53f7e41a5fad
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-10-29 14:01:16 +0530

    Error resolved

--- varchas_iitj/urls.py
+++ varchas_iitj/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from dango.http import HttpResponse
+from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
